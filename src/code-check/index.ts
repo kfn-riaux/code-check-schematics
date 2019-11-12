d1fdaa161f2cccac9b91e43b183078a8d2dc98f0
KadoTomohiro <kado.tomohiro@jp.fujitsu.com>
2019-11-12 10:22:24 +0900

    separate method and settings

--- src/code-check/index.ts
+++ src/code-check/index.ts
@@ -10,63 +10,15 @@ import {
 } from '@angular-devkit/schematics';
 import {Schema} from './schema';
 import {strings} from '@angular-devkit/core';
-import {addPackageJsonDependency, NodeDependency, NodeDependencyType} from '@schematics/angular/utility/dependencies';
-
-
-const haskySetting = {
-    'hooks': {
-        'pre-commit': 'lint-staged'
-    }
-};
-const lintStagedSetting = {
-    'src/**/*.@(ts|html|css|scss)': [
-        'npm run format',
-        'npm run lint',
-        'git add'
-    ]
-};
-
-const additionalScripts = {
-    'lint': 'npm run lint:ts && npm run lint:style',
-    'lint:ts': 'ng lint',
-    'lint:style': 'stylelint "src/**/*.(css|scss)"',
-    'format': 'prettier --config .prettierrc.json --ignore-path .prettierignore --write',
-};
-
-const devDependencies: NodeDependency[] = [
-    {name: 'husky', version: '^2.2.0', type: NodeDependencyType.Dev},
-    {name: 'lint-staged', version: '^8.1.6', type: NodeDependencyType.Dev},
-    {name: 'prettier', version: '^1.17.0', type: NodeDependencyType.Dev},
-    {name: 'stylelint', version: '^10.0.1', type: NodeDependencyType.Dev},
-    {name: 'stylelint-config-prettier', version: '^5.1.0', type: NodeDependencyType.Dev},
-    {name: 'stylelint-config-standard', version: '^18.3.0', type: NodeDependencyType.Dev},
-    {name: 'stylelint-scss', version: '^3.6.1', type: NodeDependencyType.Dev},
-    {name: 'tslint-config-prettier', version: '^1.18.0', type: NodeDependencyType.Dev},
-]
+import {addPackageJsonDependency} from '@schematics/angular/utility/dependencies';
+import {additionalScripts, devDependencies, haskySetting, lintStagedSetting} from './settings';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function codeCheck(_options: Schema): Rule {
     return (tree: Tree, _context: SchematicContext) => {
 
-        const packageJsonBuffer = tree.read('package.json');
-        if (!packageJsonBuffer) {
-            throw new SchematicsException('Not npm project');
-        }
-        const packageJson = JSON.parse(packageJsonBuffer.toString());
-
-        for (let devDependency of devDependencies) {
-            addPackageJsonDependency(tree, devDependency);
-        }
-
-        packageJson.hasky = haskySetting;
-        packageJson['lint-staged'] = lintStagedSetting;
-        packageJson.scripts = {
-            ...packageJson.scripts,
-            ...additionalScripts
-        };
-        tree.overwrite('package.json', JSON.stringify(packageJson, null, 2));
-
+        updatePackageJson(tree);
 
         const sourceTemplates = url('./files');
         const sourceParametrizedTemplates = apply(sourceTemplates, [
@@ -77,4 +29,24 @@ export function codeCheck(_options: Schema): Rule {
         ]);
         return mergeWith(sourceParametrizedTemplates)(tree, _context);
     };
+}
+
+function updatePackageJson(tree: Tree): void {
+    const packageJsonBuffer = tree.read('package.json');
+    if (!packageJsonBuffer) {
+        throw new SchematicsException('Not npm project');
+    }
+    const packageJson = JSON.parse(packageJsonBuffer.toString());
+
+    for (let devDependency of devDependencies) {
+        addPackageJsonDependency(tree, devDependency);
+    }
+
+    packageJson.hasky = haskySetting;
+    packageJson['lint-staged'] = lintStagedSetting;
+    packageJson.scripts = {
+        ...packageJson.scripts,
+        ...additionalScripts
+    };
+    tree.overwrite('package.json', JSON.stringify(packageJson, null, 2));
 }
---
+++ src/code-check/settings.ts
@@ -0,0 +1,32 @@
+import {NodeDependency, NodeDependencyType} from '@schematics/angular/utility/dependencies';
+
+export const haskySetting = {
+    'hooks': {
+        'pre-commit': 'lint-staged'
+    }
+};
+export const lintStagedSetting = {
+    'src/**/*.@(ts|html|css|scss)': [
+        'npm run format',
+        'npm run lint',
+        'git add'
+    ]
+};
+
+export const additionalScripts = {
+    'lint': 'npm run lint:ts && npm run lint:style',
+    'lint:ts': 'ng lint',
+    'lint:style': 'stylelint "src/**/*.(css|scss)"',
+    'format': 'prettier --config .prettierrc.json --ignore-path .prettierignore --write',
+};
+
+export const devDependencies: NodeDependency[] = [
+    {name: 'husky', version: '^2.2.0', type: NodeDependencyType.Dev},
+    {name: 'lint-staged', version: '^8.1.6', type: NodeDependencyType.Dev},
+    {name: 'prettier', version: '^1.17.0', type: NodeDependencyType.Dev},
+    {name: 'stylelint', version: '^10.0.1', type: NodeDependencyType.Dev},
+    {name: 'stylelint-config-prettier', version: '^5.1.0', type: NodeDependencyType.Dev},
+    {name: 'stylelint-config-standard', version: '^18.3.0', type: NodeDependencyType.Dev},
+    {name: 'stylelint-scss', version: '^3.6.1', type: NodeDependencyType.Dev},
+    {name: 'tslint-config-prettier', version: '^1.18.0', type: NodeDependencyType.Dev},
+]
